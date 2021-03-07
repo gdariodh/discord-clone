a42cd0514fc738a87565c89e15236ef631f9c813
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-03-07 01:51:19 -0300

    new things in sidebar :)

---
+++ src/components/sidebar/SidebarChannels.js
@@ -0,0 +1,14 @@
+import "./sidebar.css";
+
+const SidebarChannels = ({ id, channel }) => {
+  return (
+    <div className="sidebarChannels">
+      <h4>
+        <span className="sidebarChannel_hash">#</span>
+        Youtube
+      </h4>
+    </div>
+  );
+};
+
+export default SidebarChannels;
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,51 @@
 // css component
-import "./sidebar.css" 
+import "./sidebar.css";
+// icons
+import { ExpandMore, Add, SignalCellularAlt } from "@material-ui/icons";
+// components
+import SidebarChannels from "./SidebarChannels"
+
+// FIXME: ME QUEDE EN 1:01 HRS
 
 const Sidebar = () => {
-    return ( 
-        <aside className='sidebar'>
-            <p>Im the sidebar</p>
+  return (
+      <>
+      {/* usamos BEM */}
+    <aside className="sidebar">
+      <div className="sidebar__top">
+        <h3>Discord Clone by gdariodh </h3>
+        <ExpandMore className="sidebar__expandMore" />
+      </div>
+      <div className="sidebar__channel">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMore className="sidebar__expandMore" />
+            <h4>Text Channels</h4>
+          </div>
 
-         <div className="sidebar__top">
-             <h3>skere</h3>
-         </div>
-              
-        </aside>
-     );
-}
- 
+          <Add className="sidebar__addChannel" />
+        </div>
+
+        <div className="sidebar__channelsList">
+            <SidebarChannels/>
+            <SidebarChannels/>
+            <SidebarChannels/>
+            <SidebarChannels/>
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAlt className="sidebar__voiceIcon"
+        fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+           <h3>Voice Connected</h3>
+            <p>Stream</p>
+        </div>
+      </div>
+    </aside>
+    </>
+  );
+};
+
 export default Sidebar;
