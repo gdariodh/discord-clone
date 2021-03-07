e4ae1a9e57f09e783503c17c028ca89c18195c96
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-03-07 00:33:48 -0300

    start :)

---
+++ src/components/chat/index.js
@@ -0,0 +1,12 @@
+// css component
+import './chat.css'
+
+const Chat = () => {
+    return ( 
+        <main>
+            <h1>Chat discord {":)"}</h1>
+        </main>
+     );
+}
+ 
+export default Chat;
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,17 @@
+// css component
+import "./sidebar.css" 
+
+const Sidebar = () => {
+    return ( 
+        <aside className='sidebar'>
+            <p>Im the sidebar</p>
+
+         <div className="sidebar__top">
+             <h3>skere</h3>
+         </div>
+              
+        </aside>
+     );
+}
+ 
+export default Sidebar;
