046fee4ea7dac6905d97832909270d5a80e86bdc
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-03 01:11:48 -0300

    conditional component Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // components
-import Sidebar from './components/sidebar'
-import Chat from "./components/chat"
+import Sidebar from "./components/sidebar";
+import Chat from "./components/chat";
+import Login from "./components/login"
+
+// useSelector accede state, useDispatch invoca actions
+import { useSelector } from "react-redux";
+// states de user
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  // option1: const user = useSelector(state => state.user.user)
+
+  // option2:
+  const user = useSelector(selectUser);
+  console.log(user);
+
   return (
     <>
-    <div className="app">
-       {/* sidebar component */}
-       <Sidebar/>
-       {/* chat component */}
-       <Chat/>
-    </div> 
+      <div className="app">
+        {user ? (
+          <>
+            <Sidebar />
+            <Chat />
+          </>
+        ) : (
+          <Login />
+        )}
+      </div>
     </>
   );
 }
