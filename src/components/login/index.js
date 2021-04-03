c3399fb7ca5c900a65e1f57026f2f3cf85492768
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-03 01:12:24 -0300

    starting auth with google provider

---
+++ src/components/login/index.js
@@ -0,0 +1,36 @@
+import "./login.css";
+// firebase es nuestra instancia propia donde estan metodos, y conexion de la app con la api
+import {auth, provider} from '../../firebase'
+
+const Login = () => {
+   const loginWithGoogle = () => {     
+     auth.signInWithPopup(provider).catch(err => console.log(err.message))
+   }
+
+
+  return (
+    <div className="login">
+      <div className="login__card">
+        <div className="login__cardHeader">
+          <h3>Discord clone</h3>
+          <div className="login__img">
+            <img src="https://www.pdvg.it/wp-content/uploads/2021/03/Discord.jpeg" alt="discord icon" />
+          </div>
+        </div>
+        <div className="login__cardBody">
+              <div className="login__cardBodyGoogle">
+              <h4>Login with</h4>
+              <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_200_200/0/1519856215226?e=2159024400&v=beta&t=r--a5-Dl4gvVE-xIkq8QyBzZ8mQ-OYwBOrixNzR95H0" alt="google icon"/>
+              </div>
+            <button onClick={loginWithGoogle}>Login</button>
+        </div>
+
+        <div className="login__cardFooter">
+            <p>by gdariodh</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
