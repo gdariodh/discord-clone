b3fd2fed2ace0150c0346b2a1a9139b10fd46af0
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-03 20:38:09 -0300

    add onAuthStateChanged

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 // components
 import Sidebar from "./components/sidebar";
 import Chat from "./components/chat";
-import Login from "./components/login"
-
+import Login from "./components/login";
 // useSelector accede state, useDispatch invoca actions
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // states de user
 import { selectUser } from "./features/userSlice";
+// actions de user
+import { login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
-  // option1: const user = useSelector(state => state.user.user)
+  const dispatch = useDispatch();
 
   // option2:
   const user = useSelector(selectUser);
-  console.log(user);
+  // option1: const user = useSelector(state => state.user.user)
+
+  useEffect(() => {
+    // revisa si hay un auth
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        dispatch(login({
+          // action.payload
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName
+        }));
+      }else{
+        // pone user como null
+        dispatch(logout())
+      }
+    });
+  }, [dispatch]);
 
   return (
     <>
