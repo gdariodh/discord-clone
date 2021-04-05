c9461a64a706fea85529f3413564ee7673d4e269
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-04 21:01:20 -0400

    add messages logic

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -1,20 +1,25 @@
-import './message.css'
+import "./message.css";
 // icons
 import { Avatar } from "@material-ui/core";
 
+const Message = ({ message }) => {
+  const { user, timestamp } = message;
+  return (
+    <div className="message">
+      <Avatar src={user.photo} />
+      <div className="message__info">
+        <h4>
+          {" "}
+          {user.displayName}
+          <span className="message__timestamp">
+            {/* transformar timestamp de firebase a fecha */}
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
+        <p>{message.message}</p>
+      </div>
+    </div>
+  );
+};
 
-const Message = () => {
-    return ( 
-        <div className='message'>
-            <Avatar/>
-            <div className="message__info">
-                <h4>Gdariodh
-                  <span className="message__timestamp">this is a timestamp</span>
-                </h4>
-                <p>This is a message</p>
-            </div>
-        </div>
-     );
-}
- 
 export default Message;
--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,41 +1,117 @@
+import React, { useEffect, useState } from "react";
 // css component
-import './chat.css'
+import "./chat.css";
 // components
-import ChatHeader from './ChatHeader'
-import Message from './Message'
+import ChatHeader from "./ChatHeader";
+import Message from "./Message";
 // icons
-import {AddCircle, CardGiftcard, Gif, EmojiEmotions} from '@material-ui/icons'
-
-// FIXME: me quede en el minuto 1:35hrs - estabamos empezando a crear el componente de Message
-
+import {
+  AddCircle,
+  CardGiftcard,
+  Gif,
+  EmojiEmotions,
+} from "@material-ui/icons";
+// redux
+import { selectUser } from "../../features/userSlice";
+import { selectChannelId, selectChannelName } from "../../features/appSlice";
+import { useSelector } from "react-redux";
+// firebase
+import db from "../../firebase";
+import firebase from "firebase";
 
 const Chat = () => {
-    return ( 
-        <main className='chat'>
-            <ChatHeader/>
+  const user = useSelector(selectUser);
+  const channelName = useSelector(selectChannelName);
+  const channelId = useSelector(selectChannelId);
 
-            <div className="chat__messages">
-               <Message/>
-               <Message/>
-               <Message/>
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    //   obtener msgs
+
+    // agregamos condicional para evitar error
+    if (channelId) {
+      db.collection("channels")
+        // indicamos cual channel queremos mediante el id del channel, para filtrar :)
+        .doc(channelId)
+        // nos vamos a la subCollection "messages" de la collection "channels"
+        .collection("messages")
+        // ordenamos por fecha de entrada
+        .orderBy("timestamp", "desc")
+        // accedemos a la info
+        .onSnapshot((snapshot) =>
+          // add data al state, doc.data() accede a la data
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [input, channelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (!input) alert("Type a message :)");
+
+    // agregar el nuevo mensaje a la subCollection "messages"
+    db.collection("channels").doc(channelId).collection("messages").add({
+      message: input,
+      // en vez de usar new Date(), usamos este method de firebase para las fechas :)
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user,
+    });
+    // TODO: si no existe la subCollection de message, la crea y le agrega el nuevo mensaje :)
+
+    setInput("");
+
+    console.log(messages);
+  };
+
+  return (
+    <main className="chat">
+      {channelId ? (
+        <>
+          <ChatHeader channelName={channelName} />
+
+          <div className="chat__messages">
+            {messages.length > 0 && messages.map((message, id) => (
+              <Message
+                key={id}
+                message={message}
+              />
+            ))}
+          </div>
+
+          <div className="chat__input">
+            <AddCircle fontSize="large" />
+            <form onSubmit={sendMessage}>
+              <input
+                onChange={(e) => setInput(e.target.value)}
+                type="text"
+                value={input}
+                placeholder={`Message #${channelName}`}
+              />
+              <button className="chat__inputButton" type="submit">
+                Send Message
+              </button>
+            </form>
+
+            <div className="chat__inputIcons">
+              <CardGiftcard />
+              <Gif />
+              <EmojiEmotions />
             </div>
+          </div>
+        </>
+      ) : (
+        <div className="chat__alert">
+          <h3 className="chat__alertTitle">Hi welcome to the chat!!! 🦁</h3>
+          <p className="chat__alertText">
+            Select or create a channel for start ⏩
+          </p>
+        </div>
+      )}
+    </main>
+  );
+};
 
-            <div className="chat__input">
-               <AddCircle fontSize='large'/>
-               <form>
-                   <input type="text" placeholder={`Message #TESTCHANNEL`}/>
-                   <button className="chat__inputButton" type='submit'>Send Message</button>
-               </form>
-
-               <div className="chat__inputIcons">
-                   <CardGiftcard/>
-                   <Gif/>
-                   <EmojiEmotions/>
-               </div>
-            </div>
-            
-        </main>
-     );
-}
- 
 export default Chat;
